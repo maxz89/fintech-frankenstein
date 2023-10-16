276c4211f1d76636ba6ab2772c6dc8097341ce2d
akaashdash <akaash.dash@gmail.com>
2023-10-16 15:38:51 -0400

    Resolve dependency issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './styles\\App.css';
+import './styles/App.css';
 import ChatPrompt from "./components/chatPrompt";
 
 function App() {
